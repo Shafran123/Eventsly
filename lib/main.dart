8fae580d85f9b57cf82afbacc97407e61526d51c
Shafran <Shafran@ieee.org>
2019-03-13 13:39:31 +0530

    Hello Eventsly

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+      "Hello Eventsly",
+      textDirection: TextDirection.ltr,
+      ))
+    );
+}
