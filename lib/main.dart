ad46813f23404471008ed6df9b6df508614b78cf
Shafran <Shafran@ieee.org>
2019-03-13 14:04:26 +0530

    Hello Eventsly

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Center(
-      child: Text(
-      "Hello Eventsly",
-      textDirection: TextDirection.ltr,
-      ))
-    );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Hello Eventsly",
+    home: Material(
+      color:  Colors.white,
+      child: Center(
+        child: Text(
+          "Hello Eventsly",
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    ),
+  ));
 }
+
