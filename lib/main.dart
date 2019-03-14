f59b28dc81b986115dc85a153d13e0e220c73e7c
Shafran <Shafran@ieee.org>
2019-03-14 10:42:10 +0530

    Hello Eventsly

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Hello Eventsly",
-    home: Material(
-      color:  Colors.white,
-      child: Center(
-        child: Text(
-          "Hello Eventsly",
-          textDirection: TextDirection.ltr,
-        ),
-      ),
-    ),
-  ));
-}
+void main() => runApp(MyApp());
 
+// the root widget of our application
+class MyApp extends StatelessWidget {
+  String get data => null;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body:  Container(
+            alignment: Alignment.bottomLeft,
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                  image: new AssetImage('images/Path1.png'),
+                  alignment: Alignment.topLeft
+                  // fit: BoxFit.cover,
+                  ),
+            ),
+            child: new Text('Eventsly',
+                style: new TextStyle(
+                    fontSize: 70.0,
+                    fontFamily: 'Cocogoose Pro',
+                    color: Colors.white)
+                    ),
+                 
+                    padding: EdgeInsets.only(bottom: 675.0),  
+                    
+            ),
+          )
+        );
+  }
+}
