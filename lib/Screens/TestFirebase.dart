53777f906cf24185b643bc9a82fffd06ede919ed
Shafran <Shafran@ieee.org>
2019-03-23 16:08:06 +0530

    Hello Eventsly

--- lib/Screens/TestFirebase.dart
+++ lib/Screens/TestFirebase.dart
@@ -11,7 +11,7 @@ class HomeTest extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'FibaseTest',
       theme: ThemeData(
-      primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue,
       ),
       home: HomePage(
         title: 'Firebase Test',
@@ -34,23 +34,27 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-         currentIndex: 0,
-         items:[
-           BottomNavigationBarItem(
-             icon: Icon(Icons.calendar_today),
-             title: Text("Up Coming")
-           ),
-           BottomNavigationBarItem(
-             icon: Icon(Icons.mail),
-             title: Text("Invited")
-           ),
-           BottomNavigationBarItem(
-             icon: Icon(Icons.collections_bookmark),
-             title: Text("Went")
-           )
-         ],
-       ),
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today), title: Text("Up Coming")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.mail), title: Text("Invited")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.collections_bookmark), title: Text("Went"))
+        ],
+      ),
       appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                debugPrint('hi');
+              },
+            );
+          },
+        ),
         title: Text(widget.title),
       ),
       body: ListPage(),
@@ -88,26 +92,110 @@ class _ListPageState extends State<ListPage> {
             return ListView.builder(
               itemCount: snapshot.data.length,
               itemBuilder: (_, index) {
-                return Card(
-                  child: Column(
-                    children: <Widget>[
-                       Image.network(snapshot.data[index].data['cover_img']),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child:   Text(
-                        snapshot.data[index].data['title'],
-                        style: TextStyle(
-                          fontFamily: 'Cocogoose Pro',
-                          fontSize: 20.0
+                return Padding(
+                  padding:
+                      const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+                  child: Container(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
+                      elevation: 4.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(15.0),
+                                topRight: Radius.circular(15.0)),
+                            child: Image.network(
+                                snapshot.data[index].data['cover_img']),
                           ),
+                          Container(
+                            child: Row(
+                              children: <Widget>[
+                                ButtonTheme(
+                                  minWidth: 350.0,
+                                  height: 30.0,
+                                  child: RaisedButton(
+                                    child: Text(
+                                      snapshot.data[index].data['Date']
+                                          .toString(),
+                                      style: TextStyle(
+                                          fontFamily: 'Product Sans',
+                                          fontSize: 20.0,
+                                          color: Colors.white),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0,bottom: 3.0),
+                            child: Text(
+                              'Orgrazied by : ' +
+                                  snapshot.data[index].data['organizer'],
+                              style: TextStyle(fontFamily: 'Product Sans Re'),
+                            ),
+                          ),
+
+                          Row(
+                            children: <Widget>[
+                              Column(
+                                children: <Widget>[
+                                  Container(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(left: 8.0,bottom: 3.0),
+                                      child: Text(
+                                        snapshot.data[index].data['title'],
+                                        style: TextStyle(
+                                            fontFamily: 'Product Sans',
+                                            fontSize: 25.0,
+                                          
+                                            ),
+                                            
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0,bottom: 3.0),
+                            child: Text(
+                             
+                                  snapshot.data[index].data['short_desc'],
+                              style: TextStyle(fontFamily: 'Product Sans Re'),
+                            ),
+                          ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ButtonTheme(
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                                    minWidth: 350.0,
+                                    
+                                    child: RaisedButton(
+                                      child: Text(
+                                        'Rsvp Now !',
+                                        style: TextStyle(
+                                            fontFamily: 'Product Sans',
+                                            fontSize: 20.0,
+                                            color: Colors.white),
+                                      ),
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                            ),
+                          //Image.file(snapshot.data[index].data['cover_img']),
+
+                          // Image.network('https://i.imgur.com/wnwXIim.png')
+                          
+                        ],
                       ),
-                      ),
-                      //Text(snapshot.data[index].data['cover_img']),
-                      //Image.file(snapshot.data[index].data['cover_img']),
-                     
-                    
-                      // Image.network('https://i.imgur.com/wnwXIim.png')
-                    ],
+                    ),
                   ),
                 );
               },
